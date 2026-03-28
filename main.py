272f66b39e6818233c979695b4bca8dc5ca742e0
rustboar <gitlab.com.untrue805@passmail.net>
2026-03-28 11:18:18 -0400

    Start

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+
+def main():
+    print("Starting Asteroids with pygame version: VERSION")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while pygame.init():
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    screen.fill("black")
+
+
+if __name__ == "__main__":
+    main()
