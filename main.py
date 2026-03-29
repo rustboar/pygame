f434e20cfb50acc4431d339c546e0150f897a932
rustboar <gitlab.com.untrue805@passmail.net>
2026-03-28 22:10:18 -0400

    removed 3 print statements that i should have awhile ago

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ from shot import Shot
 
 
 def main():
-    print("Starting Asteroids with pygame version: VERSION")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
